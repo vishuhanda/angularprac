21b1308fbc90074e2d9ecd81b4a5dcf5ff82911e
vishuhanda <itsvishuhanda@gmail.com>
2024-01-09 13:07:15 +0530

    updated app.module.ts

--- angularprac/src/app/app.module.ts
+++ angularprac/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ActivitypageComponent } from './activitypage/activitypage.component';
@@ -14,8 +13,18 @@ import { ProfilepageComponent } from './profilepage/profilepage.component';
 import { InboxpageComponent } from './inboxpage/inboxpage.component';
 import { SettingspageComponent } from './settingspage/settingspage.component';
 import { SearchjobspageComponent } from './homepage/searchjobspage/searchjobspage.component';
-import { SearchtabpageComponent } from './homepage/searchjobspage/searchtabpage/searchtabpage.component';
 
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTuno_tWZ17imxnFgPNi8AYdgx22BcyHs",
+  authDomain: "angularprac-2a989.firebaseapp.com",
+  projectId: "angularprac-2a989",
+  storageBucket: "angularprac-2a989.appspot.com",
+  messagingSenderId: "766069400814",
+  appId: "1:766069400814:web:7cb719d2fd336f0ff63fff",
+  measurementId: "G-73N5LY7G3R"
+};
 
 @NgModule({
   declarations: [
@@ -28,8 +37,7 @@ import { SearchtabpageComponent } from './homepage/searchjobspage/searchtabpage/
     ProfilepageComponent,
     InboxpageComponent,
     SettingspageComponent,
-    SearchjobspageComponent,
-    SearchtabpageComponent
+    SearchjobspageComponent
   ],
   imports: [
     BrowserModule,
